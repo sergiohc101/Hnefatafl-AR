8bbb4cf6b69569a33274833a1e571731eb56458d
Jorge A. Barba <jart.barba@gmail.com>
2015-10-05 22:41:11 -0500

    Componentes de Config. añadidos
    Se añadieron los Toggle (checkboxes) y el slide, tienen funcionalidad
    pero no la correspondiente a su objetivo en el juego.

---
+++ Assets/Scripts/ToggleMusic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class ToggleMusic : MonoBehaviour {
+	
+	
+	public Toggle musicToggle; //Drag and drop your toggle game object here or you can even get a reference to this in Start()
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetMusic()
+	{
+		if(musicToggle.isOn) {
+			// Set music On
+			gameObject.audio.Play();
+
+		} else {
+			// Set music Off
+			gameObject.audio.Stop();
+		}
+	}
+}
